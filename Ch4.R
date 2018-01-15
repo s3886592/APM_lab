40eb736bdac4efa0c15b5aa17a46fb8e99e4bec5
s3886592 <yyang90@connect.hku.hk>
2018-01-16 01:51:35 +0800

    update Ch4 labs

--- Ch4.R
+++ Ch4.R
@@ -6,5 +6,37 @@ library(dplyr)
 library(ipred)
 
 # Labs --------------------------------------------------------------------
+data(twoClassData)
+
+set.seed(1)
+
+trainingRows <- createDataPartition(classes, p = 0.8, list = F)
+head(trainingRows)
+
+trainPredictors <- predictors[trainingRows,]
+trainClasses <- classes[trainingRows]
+testPredictors <- predictors[-trainingRows,]
+testClasses <- classes[-trainingRows]
+
+
+repeated_splits <- createDataPartition(classes, p = 0.8, times = 3)
+
+
+
+createFolds(trainClasses, k = 10, returnTrain = T)
+createMultiFolds(trainClasses, k = 5, time = 2)
+
+trainPredictors <- as.matrix(trainPredictors)
+knnFit <- knn3(x = trainPredictors, y = trainClasses, k = 5)
+knnFit
+predict(knnFit, newdata = testPredictors, type = "class")
+
+
+
+
+
+
+
+
 
 
